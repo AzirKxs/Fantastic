170d2de3f1d01227ca03b2a3b3e7b36c983ebff6
AzirKxs <Kxs666666>
2021-05-20 13:21:11 +0800

    first commit

---
+++ C++学习/Project2/Project2/源.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	cout << 'w'<<'\a'<<'w'<< endl; 
+	return 0;
+}
---
+++ C++学习/projectC++/projectC++/源1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	cout << "Hello C++" << endl;
+	int b;
+	cout << "请输入一个整数：" << endl;
+	cin >> b;
+	cout << "你输入的数字为：" << b << endl; \
+	return 0;
+}
